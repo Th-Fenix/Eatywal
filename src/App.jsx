7d1763fae2d48ea68ca9991558fa7ee5f7c566a0
th_fenix <thefenixbz@gmail.com>
2024-08-29 18:05:49 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+import '@fontsource-variable/montserrat';
+function App() {
+
+  return (
+    <>
+      <h1 className='text-blue-800 text-5xl font-bold'>Hola Mundo prro</h1>
+    </>
+  )
+}
+
+export default App
